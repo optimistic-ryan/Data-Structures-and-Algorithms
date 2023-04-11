e239b8be3e775e3d11c83c0d061501e3db557e76
optimistic-ryan <106937234+optimistic-ryan@users.noreply.github.com>
2023-04-11 01:53:03 -0400

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,13 +1,83 @@
-from collections import defaultdict
-
-def fn(arr, k):
-    counts = defaultdict(int)
-    counts[0] = 1
-    ans = curr = 0
-
-    for num in arr:
-        # do logic to change curr
-        ans += counts[curr - k]
-        counts[curr] += 1
+def fn(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            # do something
+            return
+        if arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
     
-    return ans
+    # left is the insertion point
+    return left
+
+
+# Duplicate Elements/Leftmost Insertion Point
+
+def fn(arr, target):
+    left = 0
+    right = len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
+
+
+# Duplicate Elements/Rightmost Insertion Point
+
+def fn(arr, target):
+    left = 0
+    right = len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
+
+
+# Greedy Minimum
+
+def fn(arr):
+    def check(x):
+        # this function is implemented depending on the problem
+        return BOOLEAN
+
+    left = MINIMUM_POSSIBLE_ANSWER
+    right = MAXIMUM_POSSIBLE_ANSWER
+    while left <= right:
+        mid = (left + right) // 2
+        if check(mid):
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return left
+
+
+# Greedy Maximum
+
+def fn(arr):
+    def check(x):
+        # this function is implemented depending on the problem
+        return BOOLEAN
+
+    left = MINIMUM_POSSIBLE_ANSWER
+    right = MAXIMUM_POSSIBLE_ANSWER
+    while left <= right:
+        mid = (left + right) // 2
+        if check(mid):
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return right
